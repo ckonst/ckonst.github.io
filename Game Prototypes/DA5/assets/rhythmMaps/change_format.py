95f3f9db47bbb88c0db20acb79e4f023895c7f2c
ckonst <christian.konstantinov98@gmail.com>
2022-03-02 18:32:29 -0500

    update some stuff

--- Game Prototypes/DA5/assets/rhythmMaps/change_format.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open('beatmap.json', 'r') as f:
-    beatmap = json.load(f)
-    
-onsets = beatmap['onsets']
-with open('rhythmMap1.txt', 'w+') as f:
-    f.write('\n'.join(map(str, onsets)))
